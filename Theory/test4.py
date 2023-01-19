65eb1b763623c450590eb05a0d1a48762329bac3
hanryoul1 <1228gksfbf@khu.ac.kr>
2023-01-19 17:53:41 +0900

    Theory

--- Theory/test4.py
+++ Theory/test4.py
@@ -1,6 +1,23 @@
 import os
-os.chdir(r"C:\Users\한률\Desktop\Drone_Coding")
 
-with open("test_file.txt", "w") as f:
-    f.write("알파코\n")
-    f.write("드론 코딩")
+os.chdir(r'C:\Users\한률\Desktop\Drone_Coding\Practice')
+
+while True:
+    try:
+        word = input("영어단어 : ")
+        meaning = input("그 뜻은 무엇인가요? : ")
+        with open("word.txt", "a") as f:
+            f.write(word)
+            f.write(":")
+            f.write(meaning)
+            f.write("\n")
+    except:
+        pass
+              
+    your_input = input("입력을 그만하고 싶으면 'q'키를, 그렇지 아니면 다른 키를 누르세요: ")
+    if your_input == "q":
+        break
+    else:
+        continue
+    
+print("단어를 다 입력했습니다.")
